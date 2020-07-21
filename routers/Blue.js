2b416aeae8e5593717218bd334d249efb082eeea
Justin Homsi <justin_homsi@hotmail.com>
2020-07-21 17:47:24 +0800

    Began replacing MySQL with JSON

--- routers/Blue.js
+++ routers/Blue.js
@@ -2,6 +2,7 @@ var express = require('express')
 var router = express.Router()
 const axios = require('axios');
 var mysql = require('mysql')
+const savedPosts = require('../database/blue_posts.json');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -12,15 +13,7 @@ var connection = mysql.createConnection({
 var bluePosts;
 
 router.get('/', function(req, res) {
-  var sortedPosts;
-  try {
-    connection.query('SELECT * FROM blue_posts ORDER BY created DESC', function (error, results, fields) {
-      if (error) throw error;
-      res.send(results)
-    })
-  } catch (err) {
-    console.error(err)
-  }
+  res.send(savedPosts);
 });
 
 async function getBluePosts() {
@@ -28,32 +21,34 @@ async function getBluePosts() {
     const postsFound = await axios.get('https://us.forums.blizzard.com/en/wow/groups/blizzard-tracker/posts.json')
     .then(response => {
       bluePosts = response.data;
-      addToDatabase()
+      addToDatabaseJSON()
     })
   } catch (err) {
     console.error(err)
   }
 }
 
-function addToDatabase() {
-  if (!bluePosts.length) return;
-  bluePosts.slice().reverse().forEach(post => {
-    connection.query(`SELECT * FROM blue_posts WHERE id = ${post.topic_id}`, function (error, results, fields) {
-      if (error) throw error;
-      var postDate = new Date(post.created_at);
-      var convertedTime = postDate.toJSON().slice(0, 19).replace('T', ' ');
-      var insertVars = {id: post.topic_id, title: post.title, created: convertedTime, url: post.url, user: post.user.name};
-      if (results.length > 0) {
-        var query = connection.query('REPLACE INTO blue_posts SET ?', insertVars, function (error, results, fields) {
-          if (error) throw error;
-        })
-      } else {
-        var query = connection.query('INSERT INTO blue_posts SET ?', insertVars, function (error, results, fields) {
-          if (error) throw error;
-        })
-      }
-    })
-  });
+function addToDatabaseJSON() {
+  var fs = require('fs');
+
+  bluePosts.forEach(post => {
+    var tempObj;
+    tempObj = savedPosts
+    var postDate = new Date(post.created_at);
+    var convertedTime = postDate.toJSON().slice(0, 19).replace('T', ' ');
+    var insertVars = {id: post.topic_id, title: post.title, created: convertedTime, url: post.url, user: post.user.name};
+    if (tempObj.rows.filter(e => e.id == post.topic_id).length > 0) {
+      tempObj.rows.splice(0, 1, insertVars);
+      var json = JSON.stringify(tempObj, null, 2);
+    } else {
+      tempObj.rows.push(insertVars);
+      var json = JSON.stringify(tempObj, null, 2);
+    }
+    fs.writeFileSync('./database/blue_posts.json', json, function(err) {
+      if (err) throw err;
+      console.log('complete');
+    });
+  })
 }
 
 getBluePosts();
