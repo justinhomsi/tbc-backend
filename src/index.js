b7dba03ae80a53f5cafbd0eea8c0ad59bbe7307c
Justin Homsi <justin_homsi@hotmail.com>
2020-07-21 22:04:20 +0800

    Added a Procfile

--- src/index.js
+++ src/index.js
@@ -16,7 +16,9 @@ app.use('/npc', Creatures)
 app.use('/search', Search)
 app.use('/blue', Blue)
 
+app.use(express.static('dist'))
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
