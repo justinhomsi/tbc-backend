273bee045695d4f7fe26a8061dc298815f5047ff
Justin Homsi <justin_homsi@hotmail.com>
2020-07-31 16:45:42 +0800

    Fixed Null blue posts

--- routers/Blue.js
+++ routers/Blue.js
@@ -28,6 +28,9 @@ function addToDatabaseJSON() {
   bluePosts.forEach(post => {
     var tempObj;
     tempObj = savedPosts
+    if (post.user.name == null) {
+      post.user.name = "Blizzard Entertainment";
+    }
     var insertVars = {id: post.topic_id, title: post.title, created: post.created_at, url: post.url, user: post.user.name};
     if (tempObj.rows.filter(e => e.id == post.topic_id).length > 0) {
       tempObj.rows.splice(0, 1, insertVars);
